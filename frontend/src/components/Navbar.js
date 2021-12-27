7a8a038a7c49dc869cb79d841a40b03fec60be0d
Wenye Ma <mwysjtu@gmail.com>
2021-12-27 15:53:38 -0500

    frontend part

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,42 @@
+import './Navbar.css';
+import {Link} from 'react-router-dom';
+
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* logo */}
+            <div className="navbar__logo">
+                <h2>MERN Shopping Cart</h2>
+            </div>
+
+            {/* link */}
+            <ul className="navbar__links">
+                <li>
+                    <Link to="/cart">
+                        {/* Icon */}
+                        <i className="fas fa-shopping-cart"></i>
+                        Cart
+                        <span className="cartlogo__badge">0</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                    Shop
+                    </Link>
+                </li>
+            </ul>
+
+            {/* hamburger menu */}
+            <div className="hamburger__menu">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+        </nav>
+    )
+};
+
+export default Navbar;
